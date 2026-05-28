4be159d5eefa5276375715a240b80de08e242168
Chris Gardiner-Bill <cbill.uk@gmail.com>
2026-05-28 20:39:38 +1000

    darkmode logo fix

--- app/components/Navbar.vue
+++ app/components/Navbar.vue
@@ -1,7 +1,9 @@
 <template>
     <UHeader>
     <template #title>
-        <img :src="isDark.value === 'dark' ? '/images/logo-dark.svg' : '/images/logo-light.svg'" alt="CGB Technical Solutions" class="h-12 w-auto cursor-pointer" @click="$router.push('/')" />
+         <client-only>
+            <img :src="isDark.value === 'dark' ? '/images/logo-dark.svg' : '/images/logo-light.svg'" alt="CGB Technical Solutions" class="h-12 w-auto cursor-pointer" @click="$router.push('/')" />
+        </client-only>
     </template>
   </UHeader>
 </template>
